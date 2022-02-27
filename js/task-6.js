8b3ad5c81ad407c4a25e5de430214dbd1fe6793a
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-02-27 10:57:11 +0200

    some small changes in task 6

--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,4 @@ const deliveryFee = 50;
 const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
 
 const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+console.log("message", message);
