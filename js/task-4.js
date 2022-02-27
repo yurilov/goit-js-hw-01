86a1ab021ae1a1ab22a321e50b0a4b8454157926
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-02-27 10:48:15 +0200

    completed task 4

---
+++ js/task-4.js
@@ -0,0 +1,10 @@
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+
+// Change code below this line
+const totalPrice = pricePerItem * orderedQuantity;
+console.log("totalPrice", totalPrice);
+const productName = "Droid";
+
+// Change code below this line
+const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
